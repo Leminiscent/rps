16f71b491b8d86b294de4db1fa4cfd0be7f3ba49
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-04-04 09:52:36 -0400

    Refactor player function in RPS.py to improve strategy

--- RPS.py
+++ RPS.py
@@ -1,23 +1,35 @@
 import random
 
 
-def player(prev_play, opponent_history=[]):
+def player(prev_play, opponent_history=[], my_history=[]):
     if not prev_play:
         guess = "R"
     else:
         opponent_history.append(prev_play)
 
     counter_moves = {"R": "P", "P": "S", "S": "R"}
-    if len(opponent_history) > 2:
-        if opponent_history[-1] == opponent_history[-2]:
-            guess = counter_moves[opponent_history[-1]]
+
+    # Quincy strategy counter: if the opponent follows a fixed pattern
+    if (
+        len(set(opponent_history[-5:])) <= 2
+    ):  # If the opponent is repeating or following a simple pattern
+        guess = counter_moves[prev_play]
+    else:
+        # For Abbey and Kris: Use a more complex pattern recognition and prediction
+        if len(my_history) > 2:
+            # Analyzing my own last two moves to predict what Abbey might expect
+            last_two_me = my_history[-2:]
+            if last_two_me.count(last_two_me[0]) == 2:
+                guess = counter_moves[counter_moves[last_two_me[0]]]
+            else:
+                guess = counter_moves[random.choice(["R", "P", "S"])]
         else:
+            guess = random.choice(["R", "P", "S"])
+
+        # For Mrugesh: Countering the frequency strategy by analyzing opponent's history
+        if opponent_history:
             most_common = max(set(opponent_history), key=opponent_history.count)
             guess = counter_moves[most_common]
-    else:
-        guess = random.choice(["R", "P", "S"])
 
-    if len(opponent_history) % 5 == 0:
-        guess = random.choice(["R", "P", "S"])
-
+    my_history.append(guess)
     return guess
