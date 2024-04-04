7153fb641a2e7629c5522b9ceec91425bf9a8702
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-04-04 09:49:11 -0400

    Refactor player function in RPS.py

--- RPS.py
+++ RPS.py
@@ -2,23 +2,22 @@ import random
 
 
 def player(prev_play, opponent_history=[]):
-    if prev_play == "":
-        return random.choice(["R", "P", "S"])
+    if not prev_play:
+        guess = "R"
     else:
         opponent_history.append(prev_play)
 
     counter_moves = {"R": "P", "P": "S", "S": "R"}
-
-    if len(opponent_history) <= 5:
-        return ["P", "P", "S", "S", "R"][len(opponent_history) % 5]
-
     if len(opponent_history) > 2:
-        guess = counter_moves[opponent_history[-1]]
-
-        last_two = "".join(opponent_history[-2:])
-        if last_two in ["RR", "PP", "SS"]:
-            guess = counter_moves[guess]
+        if opponent_history[-1] == opponent_history[-2]:
+            guess = counter_moves[opponent_history[-1]]
+        else:
+            most_common = max(set(opponent_history), key=opponent_history.count)
+            guess = counter_moves[most_common]
     else:
+        guess = random.choice(["R", "P", "S"])
+
+    if len(opponent_history) % 5 == 0:
         guess = random.choice(["R", "P", "S"])
 
     return guess
